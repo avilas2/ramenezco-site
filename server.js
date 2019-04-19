b65a5e32c81662ca025d371701e00602f9334a9a
avilas2 <sebas.avilatam@gmail.com>
2019-04-19 00:04:34 -0600

    create rewards program sign up

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 
-
+//Initialize App
 const app = express();
 const port = process.env.PORT || 3000;
 
+//Set views engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//Middleware
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//Public Folder
+app.use(express.static(path.join(__dirname,'public')));
+
+//Routes
 app.get('/', function(req,res){
     res.render('index', {
         title: 'Ramenezco'
+    });
+});
+
+
+
+//Rewards Routes
+app.get('/rewards/join', function(req,res){
+    res.render('rewards-join', {
+        title: 'Ramenezco Rewards   '
     });
 });
 
