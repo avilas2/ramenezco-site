57914605cb1eb5331b70eb82eae600967c371eb7
avilas2 <sebas.avilatam@gmail.com>
2019-04-19 01:32:42 -0600

    init square api

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const SquareConnect = require('square-connect');
+
 
 
 //Initialize App
 const app = express();
 const port = process.env.PORT || 3000;
+
+//Initialize Square-Connect
+const defaultClient = SquareConnect.ApiClient.instance;
+const oauth2 = defaultClient.authentications['oauth2'];
+oauth2.accessToken = 'EAAAEFCLgXer6PC-swOdL1znK_NcFTsSrym0gNMF9fe3nTu23_JU-ojsXeRB9w1J';
+
+const apiInstance = new SquareConnect.CustomersApi();
+const body = new SquareConnect.CreateCustomerRequest();
+
 
 //Set views engine
 app.set('views', path.join(__dirname, 'views'));
@@ -27,14 +38,24 @@ app.get('/', function(req,res){
     });
 });
 
-
-
 //Rewards Routes
 app.get('/rewards/join', function(req,res){
     res.render('rewards-join', {
-        title: 'Ramenezco Rewards   '
+        title: 'Ramenezco Rewards'
     });
 });
+
+app.post('/rewards/join', function(req,res){
+    res.send(req.body);
+    apiInstance.createCustomer(req.body).then(function(data){
+        console.log('API called succesfully' + data);
+
+    }, function(error){
+        console.error(error);
+    });
+});
+
+
 
 app.listen(port, function(){
     console.log('Server running on port '+port);
